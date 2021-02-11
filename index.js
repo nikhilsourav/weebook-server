563e31cc4e4a8cbbfc8e1ac582451711f18ddbf5
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-11 13:17:48 +0530

    db connected

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL =
+  'mongodb+srv://NikhilSourav:whatithinkCluster@whatithinkcluster.ln8db.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// express
+const app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// mongoose
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => app.listen(PORT, console.log(`server running on port ${PORT}`)))
+  .catch((e) => console.log(e, `db connection error`));
