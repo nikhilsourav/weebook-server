8cbd2e1cc277cdf941a906e9e94032248e5c6aeb
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-14 01:55:36 +0530

    returned db data in reverse order (based on date of creation)

--- routes/controllers.js
+++ routes/controllers.js
@@ -2,7 +2,7 @@ import PostModel from '../models/postModel.js';
 
 export const getPosts = async (req, res) => {
   try {
-    const allPosts = await PostModel.find();
+    const allPosts = await PostModel.find().sort([['_id', -1]]); // returned data sorted by creation time (new first)
     res.status(200).json(allPosts);
   } catch (error) {
     res.status(404).json({ message: error.message });
