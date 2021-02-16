efaac013708ca3fc7ed4b50fb52d2ad4ad9b3651
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-16 20:10:56 +0530

    likePost enabled

--- routes/controllers.js
+++ routes/controllers.js
@@ -40,3 +40,18 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: `Post deleted successfully!` });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+  const post = await PostModel.findById(id);
+
+  const updatedPost = await PostModel.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,12 +2,13 @@ import express from 'express';
 const router = express.Router();
 
 // import from controllers
-import { getPosts, createPost, updatePost, deletePost } from './controllers.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from './controllers.js';
 
 // All routes
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
