9dfc631373e97093c96a3068768a52c45cd6fc77
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-16 17:39:06 +0530

    update (patch) route added

--- routes/controllers.js
+++ routes/controllers.js
@@ -1,4 +1,5 @@
 import PostModel from '../models/postModel.js';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
   try {
@@ -18,4 +19,14 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostModel.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+  res.json(updatedPost);
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 // import from controllers
-import { getPosts, createPost } from './controllers.js';
+import { getPosts, createPost, updatePost } from './controllers.js';
 
 // All routes
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
